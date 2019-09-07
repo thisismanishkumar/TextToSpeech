21bed5ae2f918abd737e18b5572d0dded75d9514
Manish <manishkumar13899@gmail.com>
2019-09-07 20:32:28 +0530

    Second Commit

---
+++ TextToSpeech/app/src/main/java/com/example/a15_bs180tx/texttospeech/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.a15_bs180tx.texttospeech;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+Button b1;
+    EditText e1;
+    TextToSpeech t1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button);
+        e1=(EditText)findViewById(R.id.editText);
+        t1=new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                t1.setLanguage(Locale.ENGLISH);
+                t1.setSpeechRate(0.4f);
+
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1=e1.getText().toString();
+                t1.speak(s1,TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+    }
+}
